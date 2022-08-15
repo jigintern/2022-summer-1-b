c302d5bf1a30ae04a9adabeecf16f8414cb0779e
Shimoyama Ayumu <m19284@g.metro-cit.ac.jp>
2022-08-16 03:33:06 +0900

    #index.html + フォームの概形を作成 + デバッグ用の窓を追加
    # server.js
    + フォームの受け取り部分を作成
    + /submit にてJSON形式で確認できる

--- server.js
+++ server.js
@@ -1,25 +1,48 @@
-import { serve } from "https://deno.land/std@0.151.0/http/server.ts";
-import { serveDir } from "https://deno.land/std@0.151.0/http/file_server.ts";
+import { serve } from "https://deno.land/std@0.138.0/http/server.ts";
+import { serveDir } from "https://deno.land/std@0.138.0/http/file_server.ts";
+
+// 変数宣言 //
+let formJson = JSON.stringify({});
+
+//let userName = ""; //投稿者(名前)
+//let photo; //画像データ(暫定)
+//let latitude; //緯度
+//let longitude; //経度
+//let type = ""; //自販機の種類
+//let drinks = []; //売っている飲み物とか？
+// 更に拡張できそう
+
+console.log("Listening on http://localhost:8000");
 
 serve(async (req) => {
-  const pathname = new URL(req.url).pathname;
-  console.log(pathname);
+  const pathName = new URL(req.url).pathname;
 
-  if (req.method === "GET" && pathname === "/welcome-message") {
-    return new Response("I have a pen.");
+  if (req.method === "GET" && pathName === "/submit") {
+    return new Response(formJson, {
+      headers: { "Content-Type": "application/json" }
+    });
   }
-  if (req.method === "POST" && pathname === "/welcome-message") {
+
+  if (req.method === "POST" && pathName === "/submit") {
     const requestJson = await req.json();
-    const nextWord = requestJson.nextWord;
-    console.log(nextWord);
-    return new Response(nextWord, { status: 200 });
-  }
+    const userName = requestJson.userName;
+    const photo = requestJson.photo;
+    const latitude = requestJson.latitude;
+    const longitude = requestJson.longitude;
 
+    const result = JSON.stringify({userName, photo, latitude, longitude});
+
+    console.log("[POSTED]");
+    console.log("result: " + result);
+
+    formJson = result;
+    return new Response(result);
+  }
 
   return serveDir(req, {
     fsRoot: "public",
     urlRoot: "",
     showDirListing: true,
-    enableCors: true,
+    enableCors: true
   });
-});
+}).then(r => {});
