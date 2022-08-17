8a0b1031b334b7b4e644e7e175bd120e261845c9
Shimoyama Ayumu <m19284@g.metro-cit.ac.jp>
2022-08-17 13:32:49 +0900

    サインアップ・サインインの実装 データベースにも追加される

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 import { serveDir } from "https://deno.land/std@0.138.0/http/file_server.ts";
 import * as postgres from "https://deno.land/x/postgres@v0.14.0/mod.ts";
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 // 変数宣言
 let formJson = JSON.stringify({});
@@ -8,11 +9,10 @@ let formJson = JSON.stringify({});
 // 接続確認用
 console.log("Listening on http://localhost:8000");
 
-// データベースに接続するためのURL?
-const databaseUrl = "postgres://postgres:jigintern2022@db.tderfuecifzjrpfwsplc.supabase.co:6543/postgres";
-
-// 接続
-const pool = new postgres.Pool(databaseUrl, 3, true);
+// supabase
+const SUPABASE_URL = "https://tderfuecifzjrpfwsplc.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRkZXJmdWVjaWZ6anJwZndzcGxjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjA2MTA4MDgsImV4cCI6MTk3NjE4NjgwOH0.lP8Fxlyofo36Lfki_jsdejFZyPI-H3F4XOPXo8dABVw";
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 serve(async (req) => {
   const pathName = new URL(req.url).pathname;
@@ -42,7 +42,12 @@ serve(async (req) => {
     }
      */
 
+  // 自動販売機の情報の GET/POST
   if (pathName === "/posts") {
+    // データベースに接続するためのURL?
+    const databaseUrl = "postgres://postgres:jigintern2022@db.tderfuecifzjrpfwsplc.supabase.co:6543/postgres";
+    // 接続
+    const pool = new postgres.Pool(databaseUrl, 3, true);
     const connection = await pool.connect();
 
     try {
@@ -52,7 +57,7 @@ serve(async (req) => {
                     SELECT * FROM posts
                 `;
 
-          const body = JSON.stringify(result.rows, null, 5);
+          const body = JSON.stringify(result.rows, null, 4);
 
           return new Response(body, {
             headers: { "Content-Type": "application/json" }
@@ -87,6 +92,62 @@ serve(async (req) => {
       return new Response(`Internal Server Error\n\n${err.message}`, { status: 500 });
     } finally {
       connection.release();
+    }
+  }
+
+  // サインアップの GET/POST
+  if (pathName === "/signup") {
+    if (req.method === "GET") {
+      const user = supabase.auth.user();
+
+      const json = JSON.stringify(user);
+
+      return new Response(json);
+    }
+
+    if (req.method === "POST") {
+      const json = await req.json();
+      const email = json.email;
+      const user_id = json.user_id;
+      const password = json.password;
+
+      const { user, session, error } = await supabase.auth.signUp({
+        email: email,
+        password: password
+      },
+  {
+        data: {
+          user_id: user_id
+        }
+      });
+
+      const response = JSON.stringify({ user, session, error });
+      return new Response(response);
+    }
+  }
+
+  // サインインの GET/POST
+  if (pathName === "/signin") {
+    if (req.method === "GET") {
+      const user = supabase.auth.user();
+
+      const json = JSON.stringify(user);
+
+      return new Response(json);
+    }
+
+    if (req.method === "POST") {
+      const json = await req.json();
+      const email = json.email;
+      const password = json.password;
+
+      const { user, session, error } = await supabase.auth.signUp({
+        email: email,
+        password: password
+      });
+
+      const response = JSON.stringify({ user, session, error });
+      return new Response(response);
     }
   }
 
